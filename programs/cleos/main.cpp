f8577b6f13429c5e3b8590e4bacf12ab7cca6cd5
owenSai <yangsai@aclub.io>
2018-12-05 19:05:47 +0800

    fix vote bug

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -1008,12 +1008,12 @@ struct vote_producer_subcommand {
 	  
       add_standard_transaction_options(vote_producer);
 
-	  //asset votes = to_asset(votes_str);
+	  asset votes = to_asset(votes_str);
       vote_producer->set_callback([this] {
          fc::variant act_payload = fc::mutable_variant_object()
                   ("voter", voter_str)
                   ("producer", producer_name)
-                  ("votes", votes_str);
+                  ("votes", votes.to_string());
          send_actions({create_action({permission_level{voter_str,config::active_name}}, config::system_account_name, N(voteproducer), act_payload)});
       });
    }
